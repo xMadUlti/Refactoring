05263c8837a7db167fac034d58a72da668ea39b5
xMadUlti <alexbusch98@gmx.de>
2018-05-14 03:12:05 +0200

    Moved function 'amountFor' to Rental and changed name/parameters

--- src/Customer.java
+++ src/Customer.java
@@ -26,7 +26,7 @@ class Customer {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
 			// determine amounts for each line
-			thisAmount = amountFor(each);
+			thisAmount = each.getCharge();
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
@@ -39,26 +39,6 @@ class Customer {
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-		return result;
-	}
-
-	private double amountFor(Rental rental) {
-		double result = 0;
-		switch (rental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (rental.getDaysRented() > 2)
-				result += (rental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += rental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (rental.getDaysRented() > 3)
-				result += (rental.getDaysRented() - 3) * 1.5;
-			break;
-		}
 		return result;
 	}
 }
