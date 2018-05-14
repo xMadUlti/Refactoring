623f7539036b7e4e112d63e0d5f234acab328977
xMadUlti <alexbusch98@gmx.de>
2018-05-14 03:22:10 +0200

    Extracted 'getTotalCharge' method

--- src/Customer.java
+++ src/Customer.java
@@ -17,8 +17,17 @@ class Customer {
 		return _name;
 	};
 
+	private double getTotalCharge() {
+		double result = 0;
+		Enumeration rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+			result += each.getCharge();
+		}
+		return result;
+	}
+
 	public String statement() {
-		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		Enumeration rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
@@ -27,10 +36,9 @@ class Customer {
 			frequentRenterPoints += each.getFrequentRenterPoints();
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 		return result;
 	}
